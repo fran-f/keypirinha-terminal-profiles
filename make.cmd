23a37c1ee33bd0c29335a626ca6e2bcda0399283
Francesco Figari <hello@fran.io>
2020-05-28 18:34:40 +0100

    Enable make install command

--- make.cmd
+++ make.cmd
@@ -41,10 +41,6 @@ if "%1"=="build" (
 )
 
 if "%1"=="install" (
-    echo TODO: ensure the INSTALL_DIR variable declared at the top of this
-    echo       script complies to your configuration and remove this message
-    exit /1
-
     copy /Y "%BUILD_DIR%\*.keypirinha-package" "%INSTALL_DIR%\"
     goto end
 )
