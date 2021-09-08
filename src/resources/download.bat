6d07fe95c2162e99e13e6d52a8adc85502ac1e29
Francesco Figari <hello@fran.io>
2021-09-08 20:30:30 +0100

    Update main branch of the microsoft/terminal repository

--- src/resources/download.bat
+++ src/resources/download.bat
@@ -1,7 +1,7 @@
 @echo off
 git clone -n --depth=1 https://github.com/microsoft/terminal/
-git --git-dir=terminal/.git checkout master -- src/cascadia/CascadiaPackage/ProfileIcons/*-200.png
-git --git-dir=terminal/.git checkout master -- res/terminal/images/Square44x44Logo.targetsize-96.png
+git --git-dir=terminal/.git checkout main -- src/cascadia/CascadiaPackage/ProfileIcons/*-200.png
+git --git-dir=terminal/.git checkout main -- res/terminal/images/Square44x44Logo.targetsize-96.png
 move src\cascadia\CascadiaPackage\ProfileIcons\*-200.png .
 move res\terminal\images\Square44x44Logo.targetsize-96.png WindowsTerminal.png
 rd /s /q terminal src res
